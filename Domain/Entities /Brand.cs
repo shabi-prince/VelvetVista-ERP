1eb5bb91fb1da4dbce5e4a4c0af6b69456d01ee5
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 17:12:55 +0500

    Create Brand.cs

---
+++ Domain/Entities /Brand.cs
@@ -0,0 +1,21 @@
+﻿using Domain.AuditableEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Brand : AuditEntities
+    {
+        [Key]
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string ISONum { get; set; }
+        public string Slogan { get; set; }
+        public string LogoImg { get; set; }
+    }
+}
