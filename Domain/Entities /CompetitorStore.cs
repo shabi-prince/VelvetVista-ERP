c2eaab5e946b24fd3c7662821917aa107d6f4d30
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 17:15:36 +0500

    Create CompetitorStore.cs

---
+++ Domain/Entities /CompetitorStore.cs
@@ -0,0 +1,24 @@
+﻿using Domain.AuditableEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CompetitorStore : AuditEntities
+    {
+        [Key]
+        public int CompetitorStoreId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Address { get; set; }
+        public string MapLocation { get; set; }
+        public string DotColour { get; set; }
+        public int? CompetitorId { get; set; }
+       
+        public virtual Competitor Competitor { get; set; }
+    }
+}
